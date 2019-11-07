72bf1c104975d0e9e0b898157d10ffe0cff36a4b
Jacey Davis <jaceyd923@gmail.com>
2019-11-07 08:10:00 -0700

    finished Unit04

--- Unit04/js/main.js
+++ Unit04/js/main.js
@@ -4,10 +4,8 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
 
-let month = getRandomIntInclusive(1, 12)
-
 function getMonthName(month) {
-    let name
+    let name;
 
     switch (month) {
         case 1:
@@ -50,5 +48,39 @@ function getMonthName(month) {
         name = "Not a month"
         break;
     }
-    return name
+    return name;
 }
+
+function getFortune(fate) {
+    let message;
+
+    // on this date...
+    switch(fate) {
+        case 1:
+            message = "you will find the love of your life."
+            break;
+        case 2:
+            message = "you will discover that a close friend has been decieving you."
+            break;
+        case 3: 
+            message = "you will finally find what has been lost for so long."
+            break;
+        case 4:
+            message = "an expected visitor will bring you good news."
+            break;
+        case 5:
+            message = "misfortunes are sure to befall you."
+    }
+    return message;
+}
+
+function revealFortune() {
+    let fate = getRandomIntInclusive(1, 5)
+    let day = getRandomIntInclusive(1, 30)
+    let month = getRandomIntInclusive(1, 12)
+    const monthName = getMonthName(month)
+
+    let fortuneRevealed = `On ${monthName} ${day} ${getFortune(fate)}`
+    document.querySelector('#fortune').innerText = fortuneRevealed;
+}
+
